2b194392b1207273a2b1b65bdbd507ec3ae6f72d
luisdef <luis.assmann.1234@gmail.com>
2021-12-27 15:13:38 -0300

    build settings

--- setup.py
+++ setup.py
@@ -3,6 +3,22 @@ import os
 import zipfile
 from cx_Freeze import setup, Executable
 
+
+if sys.platform.startswith('linux'):
+    try:
+        os.system("sudo apt install python3-pip -y")
+        os.system("python3 -m pip3 install -r requirements.txt")
+        os.system("sudo apt install python3-tk")
+    except OSError as error:
+        print(error)
+
+elif sys.platform.startswith('win'):
+    try:
+        os.system("python -m pip install -r requirements.txt")
+    except OSError as error:
+        print(error)
+
+
 build_exe_options = {"packages": ["os", "sys", "tkinter", "Tcl", "tk", "mysql", "dotenv"]}
 
 base = None
