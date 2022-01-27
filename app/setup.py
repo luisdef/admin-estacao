bd09b7013376e9e81a4de040907a8c8d724edc18
luisdef <luis.assmann.1234@gmail.com>
2022-01-27 15:47:03 -0300

    settings on linux building

--- app/setup.py
+++ app/setup.py
@@ -6,14 +6,18 @@ if sys.platform.startswith('linux'):
         os.system("sudo apt install python3-pip -y")
         os.system("python3 -m pip3 install -r requirements.txt")
         os.system("sudo apt install python3-tk")
+        os.system("sudo apt install patchelf -y")
     except OSError as error:
         print(error)
 
 elif sys.platform.startswith('win'):
     try:
-        os.system("python -m pip install -r requirements.txt")
+        os.system("python -m psys.platform.startswith('win'):ip install -r requirements.txt")
     except OSError as error:
         print(error)
+
+else:
+    print("Erro!")
 
 import zipfile
 from cx_Freeze import setup, Executable
@@ -22,6 +26,7 @@ from cx_Freeze import setup, Executable
 build_exe_options = {"packages": ["os", "sys", "tkinter", "Tcl", "tk", "mysql", "dotenv"]}
 
 base = None
+icon = 'app_icon.ico'
 if sys.platform == "win32":
     base = "Win32GUI"
 
@@ -47,15 +52,19 @@ def zipdir(path, ziph):
                 )
             )
 
+p_v = str(sys.version_info[0])+'.'+str(sys.version_info[1])
 
-os.system("copy .env build\exe.win-amd64-3.10")
-os.system("copy app_icon.ico build\exe.win-amd64-3.10")
-os.system("attrib +h build\exe.win-amd64-3.10\.env && attrib +h build\exe.win-amd64-3.10\\app_icon.ico && attrib +h build\exe.win-amd64-3.10\python3.dll && attrib +h build\exe.win-amd64-3.10\python310.dll")
-os.system("ren build\exe.win-amd64-3.10 Painel-Estacao")
+if sys.platform.startswith('win'):
+    os.system(f"copy .env build\exe.win-amd64-{p_v}")
+    os.system(f"attrib +h build\exe.win-amd64-{p_v}\.env && attrib +h build\exe.win-amd64-{p_v}\\app_icon.ico && attrib +h build\exe.win-amd64-{p_v}\python3.dll && attrib +h build\exe.win-amd64-{p_v}\python310.dll")
+    os.system(f"ren build\exe.win-amd64-{p_v} Painel-Estacao")
 
-zipf = zipfile.ZipFile('Painel-Estação.zip', 'w', zipfile.ZIP_DEFLATED)
-zipdir('build\\Painel-Estacao', zipf)
-zipf.close()
+    zipf = zipfile.ZipFile('Painel-Estação.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipdir('build\\Painel-Estacao', zipf)
+    zipf.close()
 
-os.system("ren build\Painel-Estacao exe.win-amd64-3.10")
-os.system("cd build\exe.win-amd64-3.10 && PainelEstacao.exe")
+    os.system(f"ren build\Painel-Estacao exe.win-amd64-{p_v}")
+    os.system(f"cd build\exe.win-amd64-{p_v} && PainelEstacao.exe")
+
+elif sys.platform.startswith('linux'):
+    os.system(f"cp .env build/exe.linux-x86_64-{p_v}")
